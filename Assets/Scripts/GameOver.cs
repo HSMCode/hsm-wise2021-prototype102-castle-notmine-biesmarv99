fa4472f09408b788f4cd1319f10c2ea5201a42bf
biesmarv99 <92938062+biesmarv99@users.noreply.github.com>
2021-12-01 14:14:18 +0100

    Basis für Endless Runner

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Collector")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+}
