5fcf41e93399773a1d25d0cdb26f5442f9349b7a
falorac <carola.feierfeil@students.hs-mainz.de>
2021-12-06 19:07:02 +0100

    Cleaned up files and implemented spawning platforms cleaned up unused files and objects added scripts to spawn/destroy platforms and move the player added rough textures added three possible platform types that randomly spawn added ui overlay to inform players about inputs

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
-
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Collector")
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,40 +4,41 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-	[SerializeField] float jumpForce = 4f;
-	float forwardForce = 0f;
+	[SerializeField] float jumpForce = 10f;
+	public float forwardForce = 1.5f;
 
 	Rigidbody2D myBody;
 	bool canJump;
+	private float xPosition;
 
 
 	// Use this for initialization
 	void Start()
 	{
 		myBody = GetComponent<Rigidbody2D>();
+		xPosition = transform.position.x;
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Jump();
+		}
 	}
 
 	public void Jump()
 	{
+		// make sure player can only jump when touching ground
+		// if player is behind x-axis starting point, let them catch up
 		if (canJump)
 		{
 			canJump = false;
-
-			if (transform.position.x < 0)
-			{
-				forwardForce = 2f;
+			if (transform.position.x <= xPosition) {
+				myBody.velocity = new Vector2(forwardForce, jumpForce);
+			} else {
+				myBody.velocity = new Vector2(0, jumpForce);
 			}
-			else
-				forwardForce = 0f;
-
-			myBody.velocity = new Vector2(forwardForce, jumpForce);
-
 		}
 	}
 
